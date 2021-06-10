76c273fca6d6cd6b8f0e9bce1dc24c5c58139164
George Bejan <George.Bejan@cloudflight.io>
2021-06-10 21:51:51 +0300

    cors allow all origins and all methods

--- src/main/java/ro/code4/expertconsultation/authentication/config/SecurityConfig.java
+++ src/main/java/ro/code4/expertconsultation/authentication/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ro.code4.expertconsultation.authentication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .cors().configurationSource(request -> getCorsConfiguration())
                 .and()
                 .authorizeRequests()
                 .anyRequest().permitAll();
@@ -33,5 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private CorsConfiguration getCorsConfiguration() {
+        final CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+
+        return corsConfiguration;
     }
 }
