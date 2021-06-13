cfa89d365c1d3fc0e1d59f23cb4fba1841f3d1e0
Mihai Rinzis <mihai@think.rmi>
2021-06-13 23:05:42 +0300

    increase block content size

--- src/main/java/ro/code4/expertconsultation/document/model/persistence/DocumentBlock.java
+++ src/main/java/ro/code4/expertconsultation/document/model/persistence/DocumentBlock.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 public class DocumentBlock extends Identifiable {
+    @Lob
     private String content;
     private Integer index;
     @ManyToOne(fetch = FetchType.LAZY)
