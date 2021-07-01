a016b84eac74988bf27e04996e50e9dba3f60e48
George Bejan <George.Bejan@external.cloudflight.io>
2021-07-01 16:22:52 +0300

    CRUD Document blocks

---
+++ src/main/java/ro/code4/expertconsultation/document/service/DocumentBlockService.java
@@ -0,0 +1,17 @@
+package ro.code4.expertconsultation.document.service;
+
+import ro.code4.expertconsultation.document.model.dto.DocumentBlockDto;
+
+import java.util.List;
+
+public interface DocumentBlockService {
+    List<DocumentBlockDto> list(Long documentId);
+
+    DocumentBlockDto create(Long documentId, DocumentBlockDto documentBlockDto);
+
+    DocumentBlockDto get(Long documentId, Long documentBlockId);
+
+    DocumentBlockDto update(Long documentId, Long documentBlockId, DocumentBlockDto documentBlockDto);
+
+    void delete(Long documentId, Long documentBlockId);
+}
